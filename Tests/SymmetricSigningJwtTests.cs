15ee0c835c0fe7fbba7019145726557e561e9712
Kevin Herr <kherr@mediafly.com>
2019-05-25 14:07:51 -0500

    add test

--- Tests/SymmetricSigningJwtTests.cs
+++ Tests/SymmetricSigningJwtTests.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
 using System.Security.Claims;
 using System.Text;
 using Domain.Managers;
-using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json;
 using Xunit;
 
@@ -46,99 +42,6 @@ namespace Tests
                 unique_name = "alice@example.com",
                 email = "alice@example.com"
             };
-        }
-    }
-
-    public class NoneSignedJwtTests
-    {
-        [Fact]
-        public void Can_Validate()
-        {
-            var write = new ManualService();
-            var read = new IdentityModelService();
-
-            var payload = new Dictionary<string, string>()
-            {
-                { "my_email", "alice@example.com" },
-                { "admin", "true" }
-            };
-
-            var token = write.CreateToken(payload);
-
-            var readPayload = read.ValidateToken(token);
-
-            Assert.Equal(payload["my_email"], readPayload["my_email"]);
-            Assert.Equal(payload["admin"], readPayload["admin"]);
-        }
-
-        public interface IJwtService
-        {
-            string CreateToken(Dictionary<string, string> payload);
-            Dictionary<string, string> ValidateToken(string token);
-        }
-
-        public class IdentityModelService : IJwtService
-        {
-            public string CreateToken(Dictionary<string, string> payload)
-            {
-                var tokenDescriptor = new SecurityTokenDescriptor
-                {
-                    Subject = new ClaimsIdentity(payload.Select(kvp => new Claim(kvp.Key, kvp.Value)))
-                };
-
-                var handler = new JwtSecurityTokenHandler();
-                var securityToken = handler.CreateToken(tokenDescriptor);
-                return handler.WriteToken(securityToken);
-            }
-
-            public Dictionary<string, string> ValidateToken(string token)
-            {
-                var tokenValidationParameters = new TokenValidationParameters()
-                {
-                    ValidateIssuer = false,
-                    ValidateAudience = false,
-                    RequireSignedTokens = false,
-                    RequireExpirationTime = false
-                };
-
-                var handler = new JwtSecurityTokenHandler();
-
-                var principal = handler.ValidateToken(token, tokenValidationParameters, out var validatedToken);
-
-                return principal.Claims.ToDictionary(c => c.Type, c => c.Value);
-            }
-        }
-
-        public class ManualService : IJwtService
-        {
-            public string CreateToken(Dictionary<string, string> payload)
-            {
-                var header = new
-                {
-                    alg = "none"
-                };
-                var encodedHeader = Utils.ToBase64(Utils.ToJson(header));
-                var encodedPayload = Utils.ToBase64(Utils.ToJson(payload));
-
-                return $"{encodedHeader}.{encodedPayload}.";
-            }
-
-            public Dictionary<string, string> ValidateToken(string token)
-            {
-                var parts = token.Split('.');
-                var encodedHeader = parts[0];
-                var encodedPayload = parts[1];
-                var encodedSignature = parts[2];
-
-                var header = Utils.FromJson<dynamic>(Utils.FromBase64(encodedHeader));
-
-                if (header.algo != "none")
-                {
-                    throw new Exception("Expected algo=none");
-                }
-
-                return Utils.FromJson<Dictionary<string, string>>(Utils.FromBase64(encodedPayload));
-            }
         }
     }
 
