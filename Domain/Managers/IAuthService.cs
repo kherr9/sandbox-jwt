3b2c6eb110c196e702391ea002c65207561e4966
Kevin Herr <kherr@mediafly.com>
2019-05-25 13:17:25 -0500

    add

---
+++ Domain/Managers/IAuthService.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Domain.Managers
+{
+    public class JwtAuthService
+    {
+        public string SecretKey { get; set; } = "TW9zaGVFcmV6UHJpdmF0ZUtleQ==";
+
+        public bool IsTokenValid(string token)
+        {
+            var tokenValidationParameters = GetTokenValidationParameters();
+
+            var handler = new JwtSecurityTokenHandler();
+
+            try
+            {
+                var tokenValid = handler.ValidateToken(token, tokenValidationParameters, out var _);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public string GenerateToken(JwtContainerModel model)
+        {
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(model.Claims),
+                Expires = DateTime.UtcNow.AddMinutes(model.ExpiresMinutes),
+                SigningCredentials = new SigningCredentials(GetSymmetricSecurityKey(), model.SecretAlgorithm)
+            };
+
+            var handler = new JwtSecurityTokenHandler();
+            var securityToken = handler.CreateToken(tokenDescriptor);
+            var token = handler.WriteToken(securityToken);
+
+            return token;
+        }
+
+        public ClaimsPrincipal ValidateToken(string token, out SecurityToken validatedToken)
+        {
+            var tokenValidationParameters = GetTokenValidationParameters();
+
+            var handler = new JwtSecurityTokenHandler();
+
+            return handler.ValidateToken(token, tokenValidationParameters, out validatedToken);
+        }
+
+        private TokenValidationParameters GetTokenValidationParameters()
+        {
+            return new TokenValidationParameters()
+            {
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                IssuerSigningKey = GetSymmetricSecurityKey()
+            };
+        }
+
+        private SecurityKey GetSymmetricSecurityKey()
+        {
+            var key = Convert.FromBase64String(SecretKey);
+            return new SymmetricSecurityKey(key);
+        }
+    }
+
+    public class JwtContainerModel
+    {
+        public string SecretAlgorithm { get; set; } = SecurityAlgorithms.HmacSha256Signature;
+        public int ExpiresMinutes { get; set; } = 10080; // 7 days
+        public Claim[] Claims { get; set; }
+    }
+}
---
+++ Tests/SymmetricSigningJwtTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+using Domain.Managers;
+using Xunit;
+
+namespace Tests
+{
+    public class SymmetricSigningJwtTests
+    {
+        [Fact]
+        public void Can_Verify()
+        {
+            var model = new JwtContainerModel
+            {
+                Claims = new[]
+                {
+                    new Claim(ClaimTypes.Name, "Kevin Herr"),
+                    new Claim(ClaimTypes.Email, "kherr@example.com"),
+                }
+            };
+
+            var authService = new JwtAuthService();
+
+            var token = authService.GenerateToken(model);
+
+            var principal = authService.ValidateToken(token, out var validatedToken);
+        }
+    }
+}
