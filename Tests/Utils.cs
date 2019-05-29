db918e4c38df16c77719c365b4f5d66691ac7144
Kevin Herr <kherr@mediafly.com>
2019-05-29 09:01:24 -0500

    change secret length to 512 bits, recommended

--- Tests/Utils.cs
+++ Tests/Utils.cs
@@ -74,9 +74,10 @@ namespace Tests
 
         public static byte[] GenerateSecret()
         {
+            //https://auth0.com/blog/brute-forcing-hs256-is-possible-the-importance-of-using-strong-keys-to-sign-jwts/
             using (var provider = new RNGCryptoServiceProvider())
             {
-                byte[] byteArray = new byte[32];
+                var byteArray = new byte[64];
                 provider.GetBytes(byteArray);
                 return byteArray;
             }
